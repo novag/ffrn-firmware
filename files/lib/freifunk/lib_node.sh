72404c6a4588cfda8fa70af853d6063465602eb5
w0h <git@w0h.de>
2015-05-03 12:05:52 +0200

    Cleanup

--- files/lib/freifunk/lib_node.sh
+++ files/lib/freifunk/lib_node.sh
@@ -3,28 +3,28 @@
 
 # Hardware-Address according to sticker
 get_main_address() {
-    if is_dual_wifi;then
-    	get_radio_address "wlan1"
+    if is_dual_wifi; then
+        get_radio_address "wlan1"
     else
-	get_radio_address "wlan0"
+        get_radio_address "wlan0"
     fi
 }
 
-get_radio_address(){
-	ifconfig $1 | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}' | awk '{print tolower($0)}'
+get_radio_address() {
+    ip l sh $1 | awk '/ether/ {print $2}'
 }
 
 
 # Node_Id = HW-Address, alphanumeric
-get_node_id(){
-	macaddr=$(get_main_address)
-        echo ${macaddr//:/}
+get_node_id() {
+    macaddr=$(get_main_address)
+    echo ${macaddr//:/}
 }
 
 # Dual wifi? yes / no
 # More than two devies => dual wifi, too.
 # UNDEFINED, IF WIFI IS PRESENT!
-is_dual_wifi(){
-  inum=$(iw phy | grep Wiphy | wc -l)
-  test "$inum" != "1"
+is_dual_wifi() {
+    inum=$(iw phy | grep -c Wiphy)
+    test "$inum" != "1"
 }
